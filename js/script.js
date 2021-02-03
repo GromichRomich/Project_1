1b5e3bb954f700aaa87b372dfc9cd8e8ea982181
Roma <manroma266@gmail.com>
2021-02-03 11:49:10 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// const result = confirm("Are you a alive?");
+
+// console.log(result);
+
+
+// const answer = +prompt ("Do you have 18?", "18");
+// console.log(answer + 10);
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/3`);
+
+
+const user = 'roma';
+
+alert(`hello, ${user}`);
+
+
+
+
+
+const num = 12;
+
+alert(`12 ${num}`);
+
+
+
+let incr = 10,
+    decro = 10;
+
+//++incr;
+//--decro;
+
+console.log(++incr);
+console.log(--decro);
+
+
+
+
